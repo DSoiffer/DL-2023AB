db7fc0cf95eb23d6dd264f2dce6f0c0a486b5d52
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2023-11-19 14:00:24 -0500

    Graphing/fixed show acc

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+
 
 import data_loader.FashionMNIST as FashionMNIST_loader
 import data_loader.CIFAR10 as CIFAR10_loader
 import networks.FashionMNIST as FashionMNIST_networks
 import networks.CIFAR10 as CIFAR10_networks
-from setup import abs_activation, runModel, hyper_tuning, test_loop
+from setup import abs_activation, runModel, hyper_tuning, test_loop, plot
 
 # the options we have so far 
 # TODO move to another file?
@@ -20,34 +22,34 @@ options = {
     'models': {
       'basic':  FashionMNIST_networks.Basic
     },
-    'showAcc': True
+    'show_acc': True
   },
   "CIFAR10": {
     'loader': CIFAR10_loader,
     'models': {
       'standard': CIFAR10_networks.Standard
     },
-    'showAcc': True
+    'show_acc': True
   }
 }
-# TODO showAcc feature isn't used much, kinda assumed
 
 # define what you're choosing here
 data = 'CIFAR10'
 model = 'standard'
-out_file = 'res/CIFAR10_standard.json'
+out_file = 'res/CIFAR10_standard2.json'
 
 training_data, validation_data, test_data = options[data]['loader'].load(True)
 
 absModel = options[data]['models'][model](abs_activation)
 reluModel = options[data]['models'][model](nn.ReLU())
+show_acc = options[data]['show_acc']
 
 loss_fn = nn.CrossEntropyLoss() # TODO do we need to have the ability to change this?
 
-batch_sizes = [16]
-learning_rates = [1e-4, 1e-2]
-alphas = [0.001, .01]
-epochs = [1, 2]
+batch_sizes = [16]#, 32]
+learning_rates = [1e-4]#, 1e-2]
+alphas = [0.001]#, .01]
+epochs = [2]
 
 if torch.cuda.is_available(): 
   dev = "cuda:0" 
@@ -55,15 +57,23 @@ else:
   dev = "cpu" 
 device = torch.device(dev) 
 
-resAbs, hyperAbs = hyper_tuning(absModel, training_data, validation_data, loss_fn, (batch_sizes, learning_rates, alphas, epochs))
+resAbs, hyperAbs = hyper_tuning(absModel, training_data, validation_data, loss_fn, (batch_sizes, learning_rates, alphas, epochs), show_acc)
 test_dataloader = DataLoader(test_data, batch_size=hyperAbs['batch_size']) # account for batch_size as a hyperparam
-lossAbs, accAbs = test_loop(test_dataloader, absModel, loss_fn)
-print("Abs model best results: hyperparams:",hyperAbs, "model results:", resAbs, "loss:", lossAbs, "accuracy:", accAbs*100)
+if show_acc: # i don't really like this format because of duplication, but it works
+  lossAbs, accAbs = test_loop(test_dataloader, absModel, loss_fn, show_acc)
+  print("Abs model best results: hyperparams:",hyperAbs, "\nmodel results:", resAbs, "\nloss:", lossAbs, "accuracy:", accAbs*100)
+else:
+  lossAbs = test_loop(test_dataloader, absModel, loss_fn, show_acc)
+  print("Abs model best results: hyperparams:",hyperAbs, "\nmodel results:", resAbs, "\nloss:", lossAbs)
 
-resRelu, hyperRelu  = hyper_tuning(reluModel, training_data, validation_data, loss_fn, (batch_sizes, learning_rates, alphas, epochs))
+resRelu, hyperRelu  = hyper_tuning(reluModel, training_data, validation_data, loss_fn, (batch_sizes, learning_rates, alphas, epochs), show_acc)
 test_dataloader = DataLoader(test_data, batch_size=hyperRelu['batch_size'])
-lossRelu, accRelu = test_loop(test_dataloader, reluModel, loss_fn)
-print("Relu model hypertuned results: hyperparams:",hyperRelu, "model results:", resRelu, "loss:", lossRelu, "accuracy:", accRelu*100)
+if show_acc:
+  lossRelu, accRelu = test_loop(test_dataloader, reluModel, loss_fn, show_acc)
+  print("Relu model hypertuned results: hyperparams:",hyperRelu, "\nmodel results:", resRelu, "\nloss:", lossRelu, "accuracy:", accRelu*100)
+else:
+  lossRelu = test_loop(test_dataloader, reluModel, loss_fn, show_acc)
+  print("Relu model hypertuned results: hyperparams:",hyperRelu, "\nmodel results:", resRelu, "\nloss:", lossRelu)
 
 # now doing one model with the best hyperparams for the other one
 # running relu using best of abs
@@ -71,17 +81,26 @@ train_dataloader = DataLoader(training_data, batch_size=hyperAbs['batch_size'])
 val_dataloader = DataLoader(validation_data, batch_size=hyperAbs['batch_size'])
 test_dataloader = DataLoader(test_data, batch_size=hyperAbs['batch_size'])
 optimizer = torch.optim.SGD(reluModel.parameters(), lr=hyperAbs['learning_rate'], weight_decay=hyperAbs['alpha'], momentum=.5) #TODO momentum hyperparameter?
-resRelu2 = runModel(reluModel, train_dataloader, val_dataloader, optimizer, loss_fn, True, (hyperAbs['epochs'], hyperAbs['batch_size'], 3, .1))
-lossRelu2, accRelu2 = test_loop(test_dataloader, reluModel, loss_fn)
-print("Relu model based off of abs results: hyperparams:",hyperAbs, "model results:", resRelu2, "loss:", lossRelu2, "accuracy:", accRelu2*100)
+#TODO change show_acc here
+resRelu2 = runModel(reluModel, train_dataloader, val_dataloader, optimizer, loss_fn, (hyperAbs['epochs'], hyperAbs['batch_size'], 3, .1), show_acc)
+if show_acc:
+  lossRelu2, accRelu2 = test_loop(test_dataloader, reluModel, loss_fn, show_acc)
+  print("Relu model based off of abs results: hyperparams:",hyperAbs, "\nmodel results:", resRelu2, "\nloss:", lossRelu2, "accuracy:", accRelu2*100)
+else:
+  lossRelu2 = test_loop(test_dataloader, reluModel, loss_fn, show_acc)
+  print("Relu model based off of abs results: hyperparams:",hyperAbs, "\nmodel results:", resRelu2, "\nloss:", lossRelu2)
 # running abs using best of relu
 train_dataloader = DataLoader(training_data, batch_size=hyperRelu['batch_size']) 
 val_dataloader = DataLoader(validation_data, batch_size=hyperRelu['batch_size'])
 test_dataloader = DataLoader(test_data, batch_size=hyperRelu['batch_size'])
 optimizer = torch.optim.SGD(absModel.parameters(), lr=hyperRelu['learning_rate'], weight_decay=hyperRelu['alpha'], momentum=.5) #TODO momentum hyperparameter?
-resAbs2 = runModel(absModel, train_dataloader, val_dataloader, optimizer, loss_fn, True, (hyperRelu['epochs'], hyperRelu['batch_size'], 3, .1))
-lossAbs2, accAbs2 = test_loop(test_dataloader, reluModel, loss_fn)
-print("Abs model based off of relu results: hyperparams:",hyperRelu, "model results:", resAbs2,  "loss:", lossAbs2, "accuracy:", accAbs2*100)
+resAbs2 = runModel(absModel, train_dataloader, val_dataloader, optimizer, loss_fn, (hyperRelu['epochs'], hyperRelu['batch_size'], 3, .1), show_acc)
+if show_acc:
+  lossAbs2, accAbs2 = test_loop(test_dataloader, reluModel, loss_fn, show_acc)
+  print("Abs model based off of relu results: hyperparams:",hyperRelu, "\nmodel results:", resAbs2,  "\nloss:", lossAbs2, "accuracy:", accAbs2*100)
+else:
+  lossAbs2 = test_loop(test_dataloader, reluModel, loss_fn, show_acc)
+  print("Abs model based off of relu results: hyperparams:",hyperRelu, "\nmodel results:", resAbs2,  "\nloss:", lossAbs2)
 
 
 fileRes = {
@@ -90,13 +109,13 @@ fileRes = {
       'hyper': hyperAbs,
       'model results': resAbs,
       'loss': lossAbs,
-      'accuracy': accAbs * 100
+      'accuracy': None if not show_acc else accAbs * 100
     },
     'otherHypertuned': {
       'hyper': hyperRelu,
       'model results': resAbs2,
       'loss': lossAbs2,
-      'accuracy': accAbs2 * 100
+      'accuracy': None if not show_acc else accAbs2 * 100
     }
   },
   'relu': {
@@ -104,16 +123,29 @@ fileRes = {
       'hyper': hyperRelu,
       'model results': resRelu,
       'loss': lossRelu,
-      'accuracy': accRelu * 100
+      'accuracy': None if not show_acc else accRelu * 100
     },
     'otherHypertuned': {
       'hyper': hyperAbs,
       'model results': resRelu2,
       'loss': lossRelu2,
-      'accuracy': accRelu2 * 100
+      'accuracy': None if not show_acc else accRelu2 * 100
     }
   }
 }
 with open(out_file, 'w') as f:
   json.dump(fileRes, f)
 
+
+lossDict = {
+  'abs hyp trn' : resAbs['train_losses'],
+  'abs hyp val' : resAbs['val_losses'],
+  'relu hyp trn' : resRelu['train_losses'],
+  'relu hyp val' : resRelu['val_losses'],
+  'abs noth trn' : resAbs2['train_losses'],
+  'abs noth val' : resAbs2['val_losses'],
+  'relu noth trn' : resRelu2['train_losses'],
+  'relu noth val' : resRelu2['val_losses'],
+}
+
+plot(lossDict)
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import time
 import torch.nn as nn
 from torch import Tensor
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 
 def abs_activation(x, ratio=1):
@@ -57,11 +58,12 @@ def train_loop(dataloader, model, loss_fn, optimizer, batch_size):
         if batch % (150*64/batch_size) == 0:
             loss, current = loss.item(), (batch + 1) * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    return loss.item()
 
     
 
 # This function adapted from PyTorch's "Getting Started" examples
-def test_loop(dataloader, model, loss_fn, showAcc=True):
+def test_loop(dataloader, model, loss_fn, show_acc=True):
     # Set the model to evaluation mode - important for batch normalization and dropout layers
     # Unnecessary in this situation but added for best practices
     model.eval()
@@ -75,28 +77,34 @@ def test_loop(dataloader, model, loss_fn, showAcc=True):
         for X, y in dataloader:
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
-            if showAcc:
+            if show_acc:
                 correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
     test_loss /= num_batches
-    if showAcc:
+    if show_acc:
         correct /= size
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
         return test_loss, correct
     print(f"Avg loss: {test_loss:>8f} \n")
     return test_loss
 
-def runModel(model, train_dataloader, val_dataloader, optimizer, loss_fn, showAcc, hyperparameters):
+def runModel(model, train_dataloader, val_dataloader, optimizer, loss_fn, hyperparameters, show_acc=True):
     epochs, batch_size, patience, min_delta = hyperparameters
     st = time.time()
     early_stopper = EarlyStopper(patience=patience, min_delta=min_delta)
+    train_losses = []
+    val_losses = []
+    val_accuracies = []
     for e in range(epochs):
         print(f"Epoch {e+1}\n-------------------------------")
-        traj_params = train_loop(train_dataloader, model, loss_fn, optimizer, batch_size)
-        if showAcc:
-          val_loss, val_accuracy = test_loop(val_dataloader, model, loss_fn, showAcc=showAcc)
+        train_loss = train_loop(train_dataloader, model, loss_fn, optimizer, batch_size)
+        train_losses.append(train_loss)
+        if show_acc:
+          val_loss, val_accuracy = test_loop(val_dataloader, model, loss_fn, show_acc=show_acc)
+          val_accuracies.append(val_accuracy)
         else:
-          val_loss = test_loop(val_dataloader, model, loss_fn, showAcc=showAcc)
+          val_loss = test_loop(val_dataloader, model, loss_fn, show_acc=show_acc)
+        val_losses.append(val_loss)
         if early_stopper.early_stop(val_loss, model):             
           model.load_state_dict(early_stopper.min_state_dict)
           break
@@ -104,14 +112,15 @@ def runModel(model, train_dataloader, val_dataloader, optimizer, loss_fn, showAc
     elapsed_time = et - st
     res = {
         'epochs': e+1,
-        'val_loss': val_loss,
+        'train_losses': train_losses,
+        'val_losses': val_losses,
         'elapsed_time': elapsed_time
     }
-    if showAcc:
-      res['val_accuracy'] = val_accuracy
+    if show_acc:
+      res['val_accuracies'] = val_accuracies
     return res
 
-def hyper_tuning(model, train_data, val_data, loss_fn, hyperparameters):
+def hyper_tuning(model, train_data, val_data, loss_fn, hyperparameters, show_acc=True):
     batch_sizes, learning_rates, alphas, epochs = hyperparameters
     best_CE = None
     best_res = None
@@ -132,41 +141,29 @@ def hyper_tuning(model, train_data, val_data, loss_fn, hyperparameters):
                         current = {'batch_size': b, 'learning_rate': l, 'alpha': a, 'epochs': e}
                         optimizer = torch.optim.SGD(model.parameters(), lr=l, weight_decay=a, momentum=.5) #TODO momentum hyperparameter?
                         # TODO auto defining patience and min delta
-                        modelRes = runModel(model, train_dataloader, val_dataloader, optimizer, loss_fn, True, (e, b, 3, .1))
+                        modelRes = runModel(model, train_dataloader, val_dataloader, optimizer, loss_fn, (e, b, 3, .1), show_acc)
                         epochs_ran = modelRes['epochs']
-                        loss = modelRes['val_loss']
+                        loss = modelRes['val_losses'][-1]
                         if best_CE is None or best_CE > loss: # found better model
                             best_CE = loss
                             if epochs_ran < e: # if performs better at an earlier epoch, start early
                                 earlyStopped = True
-                                current[3] = epochs_ran
+                                current['epochs'] = epochs_ran
                             best_set = current
                             best_res = modelRes
     return best_res, best_set
-# def runModel(model, train_dataloader, val_dataloader, test_dataloader, optimizer, loss_fn, showAcc, hyperparameters):
-#     epochs, batch_size, patience, min_delta = hyperparameters
-#     st = time.time()
-#     early_stopper = EarlyStopper(patience=patience, min_delta=min_delta)
-#     for e in range(epochs):
-#         print(f"Epoch {e+1}\n-------------------------------")
-#         traj_params = train_loop(train_dataloader, model, loss_fn, optimizer, batch_size)
-#         if showAcc:
-#           val_loss, _ = test_loop(val_dataloader, model, loss_fn, showAcc=showAcc)
-#         else:
-#           val_loss = test_loop(val_dataloader, model, loss_fn, showAcc=showAcc)
-#         if early_stopper.early_stop(val_loss, model):             
-#           model.load_state_dict(early_stopper.min_state_dict)
-#           break
-#     res = (e+1,)
-#     if showAcc:
-#       test_loss, test_accuracy = test_loop(test_dataloader, model, loss_fn, showAcc=showAcc)
-#       res += (test_loss,)
-#       res += (test_accuracy,)
-#     else:
-#         test_loss = test_loop(test_dataloader, model, loss_fn, showAcc=showAcc)
-#         res += (test_loss,)
-#     et = time.time()
-#     elapsed_time = et - st
-#     res += (elapsed_time,)
-#     return res
-   
+
+# lossDict is a dictionary of keys that correspond to labels and values that are arrays
+def plot(lossDict):
+    # Plot the lines
+    for key, value in lossDict.items():
+        plt.plot(value, label=key)
+
+    # Add labels and title
+    plt.xlabel('Epochs')
+    plt.ylabel('Losses')
+    plt.title('Losses Over Epochs')
+    # Add legend
+    plt.legend()
+    # Show the plot
+    plt.show()
