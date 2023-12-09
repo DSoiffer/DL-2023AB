862d79f9a9e6914675382d963efd10d09d1684aa
JustinWeintraub <yoshterra1@gmail.com>
2023-12-09 15:07:12 +0000

    adding updated pkl and abs convert (plot needs to be rerun)

--- partb/abs_convert.py
+++ partb/abs_convert.py
@@ -8,8 +8,13 @@ def abs_convert(abs_model, device, abs_state, data, title, output_path):
   test_losses = [abs_state['test_losses'][-1]]
   percent_abs_list = [0]
   range_list = [*range(1, 101, 1)]
+  range_list.insert(0,0.8)
+  range_list.insert(0,0.6)
+  range_list.insert(0,0.4)
+  range_list.insert(0,0.2)
   range_list.insert(0, 0.1)
   range_list.insert(0,0.075)
+  range_list.insert(0, 0.06256)
   range_list.insert(0, 0.05)
   range_list.insert(0,.025)
   for i in range_list:
@@ -28,12 +33,12 @@ def abs_convert(abs_model, device, abs_state, data, title, output_path):
       if i == 100:
         abs_state = train(abs_model, device, data['train'], data['test'], epochs=2, gamma=.9, lr=.001, print_every=100, output_file=output_path + "abs_model.pkl")
       else:
-        abs_state = train(abs_model, device, data['train'], data['test'], epochs=2, gamma=.9, lr=.001, print_every=100)
+        abs_state = train(abs_model, device, data['train'], data['test'], epochs=1, gamma=.9, lr=.001, print_every=100)
       losses += [abs_state['losses'][-1]]
       test_losses += [abs_state['test_losses'][-1]]
   loss_dict = {
-    'abs trn': abs_state['losses'],
-    'abs test': abs_state['test_losses']
+    'abs trn': losses,
+    'abs test': test_losses
   }
   plot(loss_dict, title, output_path + "abs_percent.png", percent_abs_list, x_label="Percent abs")
   return abs_state
